44feb8eb6a12c012750cbc94eef3a99716f46ebe
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-11-23 02:17:12 +0600

    Initial FrontEnd

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class", // enable both light & dark mode using .dark class
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
